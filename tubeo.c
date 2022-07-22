432cca08e72aaa7b80c127faa0bd93df370aa098
Renzo Salcedo <salcedo_salcedo.renzo@courrier.uqam.ca>
2022-07-22 18:50:03 -0400

    ajout pipe et boucle fils/parent

--- tubeo.c
+++ tubeo.c
@@ -22,5 +22,31 @@ int main(int argc, char ** argv) {
     int first_arg = 1;
     if (argv[1][0] == '-')
         first_arg = 3;
+    int p[2];
+    pid_t pid;
+    int fd_in = 0;
+    int status = 0;
+
+    while (*cmd != NULL) {
+        pipe(p);
+        if ((pid = fork()) == -1) {
+            exit(EXIT_FAILURE);
+        } else if (pid == 0) {
+            dup2(fd_in, 0); 
+            if (*(cmd + 1) != NULL)
+                dup2(p[1], 1);
+            close(p[0]);
+            execvp(*(cmd)[0], *cmd);
+            _Exit(127);
+        } else {
+//            if (compteur == *argv - '0') {
+//                b_read = splice(fd_in, NULL, STDOUT_FILENO, NULL, 200, NULL);
+//                printf("%d", b_read);
+//            }
+            close(p[1]);
+            fd_in = p[0]; 
+            cmd++;
+        }
+    }
     return 0;
 }
