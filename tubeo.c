79fc8394c817a20cdcda64fc8ecf327562b415cd
Renzo Salcedo <salcedo_salcedo.renzo@courrier.uqam.ca>
2022-07-22 20:31:29 -0400

    ajout validation sortie correcte

--- tubeo.c
+++ tubeo.c
@@ -1,14 +1,3 @@
-/*  INF3173 - TP2
- *  Session : été 2022
- *  
- *  IDENTIFICATION.
- *
- *      Nom : Salcedo
- *      Prénom : Renzo Arturo
- *      Code permanent : SALR02089408
- *      Groupe : 40
- */
- 
 #define _GNU_SOURCE
 #include <stdio.h>
 #include <unistd.h>
@@ -17,14 +6,16 @@
 #include <string.h>
 #include <fcntl.h>
 
-int main(int argc, char ** argv) {
+#define READ_END     0
+#define WRITE_END    1
+
+int main(int argc, char *argv[]) {
     int status;
     int compteur = 0;
     int argument_n =  1 << 20;
     int nb_commandes = 1;
     int pid_enfant;
     int index_position = 0;
-    
     for (int i = 0; i < argc; ++i)
         if (argv[i][0] == ':') {
             argv[i] = NULL;
@@ -40,13 +31,12 @@ int main(int argc, char ** argv) {
         if (argv[r] == NULL)
             index[index_position++] = r + 1;
     }
-
     int pipe_precedent, pf_file_descriptor[2];
     pipe_precedent = STDIN_FILENO;
     ssize_t octets_lus = 0;
 
     for (int i = 0; i < nb_commandes; i++) {
-	pipe(pf_file_descriptor);
+        pipe(pf_file_descriptor);
         if ((pid_enfant = fork()) == 0) {
             if (pipe_precedent != STDIN_FILENO) {
                 dup2(pipe_precedent, STDIN_FILENO);  // redirect precedent vers stdin
@@ -60,11 +50,13 @@ int main(int argc, char ** argv) {
             _Exit(127);
         }
         wait(&status);
-	close(pipe_precedent); // Ferme read end du pipe precent (pas necessaire dans parent)
+        close(pipe_precedent); // Ferme read end du pipe precent (pas necessaire dans parent)
         close(pf_file_descriptor[1]); // Ferme write du pipe courrant (pas necessaire dans parent)
         pipe_precedent = pf_file_descriptor[0]; // Sauvegarde read end du peipe courrant pour utiliser dans la prochaine iteration
     }
     close(pf_file_descriptor[0]);
-    close(pf_file_descriptor[1]);  
-    return 0;
+    close(pf_file_descriptor[1]);
+    if WIFEXITED(status)
+        return WEXITSTATUS(status);
 }
+
